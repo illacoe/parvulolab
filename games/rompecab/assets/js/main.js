dced59f11639a27a5cf6df624908aec824874431
Javier Collao <jav.acollao@gmail.com>
2020-11-23 10:12:26 -0300

    fix

--- games/rompecab/assets/js/main.js
+++ games/rompecab/assets/js/main.js
@@ -1,22 +1,22 @@
 
 var correctCards = 0;
 $( init );
-// function touchHandler(event) {
-//   var touch = event.changedTouches[0];
+function touchHandler(event) {
+  var touch = event.changedTouches[0];
 
-//   var simulatedEvent = document.createEvent("MouseEvent");
-//       simulatedEvent.initMouseEvent({
-//       touchstart: "mousedown",
-//       touchmove: "mousemove",
-//       touchend: "mouseup"
-//   }[event.type], true, true, window, 1,
-//       touch.screenX, touch.screenY,
-//       touch.clientX, touch.clientY, false,
-//       false, false, false, 0, null);
+  var simulatedEvent = document.createEvent("MouseEvent");
+      simulatedEvent.initMouseEvent({
+      touchstart: "mousedown",
+      touchmove: "mousemove",
+      touchend: "mouseup"
+  }[event.type], true, true, window, 1,
+      touch.screenX, touch.screenY,
+      touch.clientX, touch.clientY, false,
+      false, false, false, 0, null);
 
-//   touch.target.dispatchEvent(simulatedEvent);
-//   event.preventDefault();
-// }
+  touch.target.dispatchEvent(simulatedEvent);
+  event.preventDefault();
+}
 
  
 function init() {
@@ -54,10 +54,10 @@ function init() {
     } );
   }
 
-  // document.addEventListener("touchstart", touchHandler, true);
-  // document.addEventListener("touchmove", touchHandler, true);
-  // document.addEventListener("touchend", touchHandler, true);
-  // document.addEventListener("touchcancel", touchHandler, true);
+  document.addEventListener("touchstart", touchHandler, true);
+  document.addEventListener("touchmove", touchHandler, true);
+  document.addEventListener("touchend", touchHandler, true);
+  document.addEventListener("touchcancel", touchHandler, true);
 }
 
 
